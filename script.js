85b5259e311332610b19373d8d9fc70831b5ace2
Your Name <you@example.com>
2021-02-15 14:38:25 +0600

    let ul document create element

--- script.js
+++ script.js
@@ -2,4 +2,6 @@ let fruit = ["Лимон", "Банан", "Яблоко", "Абрикос"];
 let ul = document.createElement('ul');
 document.body.prepend(ul);
 
-let li = document.createElement('li');
+for (let i in fruit);
+
+
