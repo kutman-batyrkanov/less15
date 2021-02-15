7b7b257ea1864bea2425510eae4d5917f7b896f1
Your Name <you@example.com>
2021-02-15 14:43:00 +0600

    document.body.prepend

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ let fruit = ["Лимон", "Банан", "Яблоко", "Абрикос"];
 let ul = document.createElement('ul');
 document.body.prepend(ul);
 
-for (let i in fruit);
+for (let i in fruit) {
+    let li = document.createElement('li');
+    li.innerText = fruit[i];
+    ul.append(li)
+}
 
+let 
 
